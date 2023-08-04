5654e088dbabc198b68c8a9a158d75356e97e7fa
Vitor Nunes <stecks10@gmail.com>
2023-08-04 09:56:40 -0300

    Aplicação com Redux - Selecionando aula atual

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,23 @@
 import ReactPlayer from "react-player";
+import { useAppSelector } from "../store";
 
 export function Video(): JSX.Element {
+  const lesson = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    const currentLesson =
+      state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+
+    return currentLesson
+  })
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         height="100%"
         width="100%"
         controls
-        url="https://www.youtube.com/watch?v=ia6pOIFGznA"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   );
