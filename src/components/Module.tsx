893fb9e5b6892229987bc5469273be776eb8420b
Vitor Nunes <stecks10@gmail.com>
2023-08-07 20:42:21 -0300

    Utilizando Async Thunks

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,7 +2,7 @@ import * as Collapsible from '@radix-ui/react-collapsible';
 
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
-import { useAppSelector } from "../store";
+import { useAppDispatch, useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
 import { play } from "../store/slices/player";
 
@@ -13,7 +13,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
 
   const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,5 +1,6 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { useAppSelector } from "..";
+import { api } from "../../lib/axios";
 
 interface Course {
   id: number;
@@ -30,9 +31,6 @@ export const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
-    start: (state, action: PayloadAction<Course>) => {
-      state.course = action.payload;
-    },
     play: (state, action: PayloadAction<[number, number]>) => {
       state.currentModuleIndex = action.payload[0];
       state.currentLessonIndex = action.payload[1];
@@ -57,10 +55,21 @@ export const playerSlice = createSlice({
       }
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(loadCourse.fulfilled, (state, action) => {
+      state.course = action.payload;
+    });
+  },
+});
+
+export const loadCourse = createAsyncThunk("player/load", async () => {
+  const response = await api.get("/courses/1");
+
+  return response.data;
 });
 
 export const player = playerSlice.reducer;
-export const { play, next, start } = playerSlice.actions;
+export const { play, next } = playerSlice.actions;
 
 export const useCurrentLesson = () => {
   return useAppSelector((state) => {
